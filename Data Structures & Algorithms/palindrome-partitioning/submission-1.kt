eb24d174d2a3c896784016240c3590914bffceb8
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-04-09 17:54:10 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun partition(s: String): List<List<String>> {
+        val res = mutableListOf<List<String>>()
+        val part = mutableListOf<String>()
+
+        fun dfs(j: Int, i: Int) {
+            if (i >= s.length) {
+                if (i == j) {
+                    res.add(part.toList())
+                }
+                return
+            }
+
+            if (isPali(s, j, i)) {
+                part.add(s.substring(j, i + 1))
+                dfs(i + 1, i + 1)
+                part.removeAt(part.size - 1)
+            }
+
+            dfs(j, i + 1)
+        }
+
+        dfs(0, 0)
+        return res
+    }
+
+    private fun isPali(s: String, l: Int, r: Int): Boolean {
+        var left = l
+        var right = r
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
