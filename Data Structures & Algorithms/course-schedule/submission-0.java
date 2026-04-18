fc55c722981899539f5ec697cdbcbc4a96cb5bd8
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-04-18 20:39:19 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,47 @@
+
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        // Initialize graph
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // Build graph + indegree
+        for (int[] p : prerequisites) {
+            int a = p[0];
+            int b = p[1];
+            graph.get(b).add(a);
+            indegree[a]++;
+        }
+
+        // Queue for BFS
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Add nodes with indegree 0
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+
+            for (int neighbor : graph.get(course)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
