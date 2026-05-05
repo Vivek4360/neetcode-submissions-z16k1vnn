f0f6205c050406bc57b0c61151e7f8e0e9260717
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-05-05 17:50:58 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+
+        if(nums.size == 1) return nums[0]
+
+        var prev2 = nums[0]
+
+        var prev1 = maxOf(nums[0], nums[1])
+
+        for(i in 2 until nums.size){
+            val current = maxOf(nums[i] + prev2, prev1)
+
+            prev2 = prev1
+            prev1 = current
+
+        }
+
+        return prev1
+
+    }
+}
