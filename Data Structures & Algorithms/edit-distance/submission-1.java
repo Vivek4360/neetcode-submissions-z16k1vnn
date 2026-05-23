4c9cbe2a8188c2f64aa92d55ad3d73c8067c05db
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-05-23 21:04:00 +0530

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        // Create DP table
+        int[][] dp = new int[m + 1][n + 1];
+        
+        // Base cases
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;  // Delete all characters from word1
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;  // Insert all characters to word1
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(
+                        dp[i - 1][j],     // Delete
+                        Math.min(
+                            dp[i][j - 1],     // Insert
+                            dp[i - 1][j - 1]  // Replace
+                        )
+                    );
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
