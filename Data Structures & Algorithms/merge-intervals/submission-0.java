07c2d752edc29e0bab1339dc1ae6bc20a52c4f1f
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-06-05 16:24:15 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        
+        // Sort intervals by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        List<int[]> result = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        result.add(currentInterval);
+        
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+            
+            // If intervals overlap, merge them
+            if (nextStart <= currentEnd) {
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                // No overlap, add new interval
+                currentInterval = interval;
+                result.add(currentInterval);
+            }
+        }
+        
+        return result.toArray(new int[result.size()][]);
+    }
+}
