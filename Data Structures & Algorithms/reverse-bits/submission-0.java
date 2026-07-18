83ccf8ca91720409552093e71b865d925d7f8b2c
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-07-18 22:33:46 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;     // Make space
+            result |= (n & 1);        // Add last bit of n
+            n >>>= 1;                 // Unsigned right shift
+        }
+
+        return result;
+    }
+}
