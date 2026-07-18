22b13e5a8265497ca5186012fa896f902292cd07
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-07-18 22:34:44 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun reverseBits(n: Int): Int {
+        var num = n
+        var result = 0
+
+        repeat(32) {
+            result = result shl 1
+            result = result or (num and 1)
+            num = num ushr 1
+        }
+
+        return result
+    }
+}
