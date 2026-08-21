44e8abad3e33c1bf52f3ad6f376aca1163f2adb6
Vivek Kumar <159692856+Vivek4360@users.noreply.github.com>
2026-08-21 22:30:49 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.kt
@@ -0,0 +1,15 @@
+class MyHashMap() {
+    private val map = IntArray(1000001) { -1 }
+
+    fun put(key: Int, value: Int) {
+        map[key] = value
+    }
+
+    fun get(key: Int): Int {
+        return map[key]
+    }
+
+    fun remove(key: Int) {
+        map[key] = -1
+    }
+}
